1292221e917852e95c5e7d1e5148788aea60bbbe
shuvo-me <shuvo.js.dev@gmail.com>
2024-07-25 10:33:11 +0600

    design: minimal sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,17 +1,32 @@
 import { Heading, Flex } from "@radix-ui/themes";
 import { TokensIcon } from "@radix-ui/react-icons";
 import React from "react";
+import Link from "next/link";
 
 const Sidebar = () => {
   return (
     <aside className="w-64 border-r border-r-gray-900 h-screen">
       <div className=" p-5 text-center">
-        <Flex gap={"1"} align={"center"}>
+        <Flex gap={"1"} align={"center"} justify={"center"}>
           <Heading size="3" className="flex items-center gap-1">
-            NextTrello
+            <Link href={"/"}>NextTrello</Link>
           </Heading>
           <TokensIcon height={20} width={20} />
         </Flex>
+      </div>
+      <div className=" mt-10 *:text-left">
+        <ul className=" space-y-1">
+          <li>
+            <Link href={"/project-1"} className="p-4 hover:bg-slate-900 block">
+              Project1
+            </Link>
+          </li>
+          <li>
+            <Link href={"/project-2"} className="p-4 hover:bg-slate-900 block">
+              Project2
+            </Link>
+          </li>
+        </ul>
       </div>
     </aside>
   );
