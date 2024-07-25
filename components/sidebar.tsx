868497d9b09fea5d3e6826fcc55950072ea661b1
shuvo-me <shuvo.js.dev@gmail.com>
2024-07-25 20:42:03 +0600

    add: sidebar showhide functionality.

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,22 @@
+"use client";
 import { Heading, Flex } from "@radix-ui/themes";
 import { TokensIcon } from "@radix-ui/react-icons";
 import React from "react";
 import Link from "next/link";
+import cn from "@/lib/cn";
+import useStore from "@/store";
 
 const Sidebar = () => {
+  const { isShowSidebar } = useStore();
   return (
-    <aside className="w-64 border-r border-r-gray-900 h-screen">
+    <aside
+      className={cn(
+        "border-r border-r-gray-900 h-screen transition duration-200 ease-linear",
+        isShowSidebar
+          ? "w-64 translate-x-0"
+          : "w-0 -translate-x-52 overflow-hidden"
+      )}
+    >
       <div className=" p-5 text-center">
         <Flex gap={"1"} align={"center"} justify={"center"}>
           <Heading size="3" className="flex items-center gap-1">
